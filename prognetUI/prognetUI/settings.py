1d04dff40473bc743b15047d419937d4fa9dc8cd
Hua Zhang <hua.zhang1@huawei.com>
2017-07-26 13:52:22 -0700

    Step 2: Add non-reactJS views

--- prognetUI/prognetUI/settings.py
+++ prognetUI/prognetUI/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'prognetUI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'prognetUI/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
